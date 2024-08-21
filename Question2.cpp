c4c6ec0a13f693e55e6dae2b97c839b11fbdf986
PrathameshPathak2233 <100769720+PrathameshPathak2233@users.noreply.github.com>
2024-08-21 19:25:59 +0530

    Solved assignment files are added

---
+++ Question2.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int x = 4 ;
+    int y = 6 ;
+    cout<<x+y;
+}
---
+++ Question6.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r = 10;
+    float circumference = 2*3.14*r;
+    cout<<circumference;
+}
